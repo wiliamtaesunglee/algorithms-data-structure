171a542f1269c2a75567006f4ac1a46b0f37f063
edmond dantés <wiliam.tae@gmail.com>
2023-11-18 16:02:42 -0300

    easy solution for fib

--- recursion/fib.js
+++ recursion/fib.js
@@ -20,4 +20,11 @@ function fib(n) {
 console.log(fib(1)); // 1
 console.log(fib(4)); // 3
 
-console.log(fib(35)); // 3
+console.log(fib(35));
+
+// easy way
+
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
